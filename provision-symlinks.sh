4ca6e0016508a27dbd13a85dafff297a1fa1d1ee
ifeelweb <info@ifeelweb.de>
2018-03-31 10:16:54 +0200

    adding symlink provisionier

---
+++ provision-symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /srv/www/wordpress-develop/public_html/src/wp-content/plugins/
+
+ln -s /vagrant/share/sources/amazonsimpleadmin/trunk amazonsimpleadmin
+ln -s /vagrant/share/sources/amazon-simple-affiliate/trunk/amazon-simple-affiliate amazon-simple-affiliate
