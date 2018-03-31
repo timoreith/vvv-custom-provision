69cba53c628ed317f48e2e9987655c5e3d483ae0
ifeelweb <info@ifeelweb.de>
2018-03-31 11:25:42 +0200

    added psn to symlinks

--- provision-symlinks.sh
+++ provision-symlinks.sh
@@ -4,3 +4,4 @@ cd /srv/www/wordpress-develop/public_html/src/wp-content/plugins/
 
 ln -s /vagrant/share/sources/amazonsimpleadmin/trunk amazonsimpleadmin
 ln -s /vagrant/share/sources/amazon-simple-affiliate/trunk/amazon-simple-affiliate amazon-simple-affiliate
+ln -s /vagrant/share/sources/post-status-notifier/trunk/post-status-notifier post-status-notifier
