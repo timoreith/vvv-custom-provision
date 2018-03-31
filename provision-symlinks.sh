2bbad61e3da30a0531c99799c48f79f4bf545dc8
ifeelweb <info@ifeelweb.de>
2018-03-31 11:45:02 +0200

    added psn-lite to symlinks

--- provision-symlinks.sh
+++ provision-symlinks.sh
@@ -5,3 +5,4 @@ cd /srv/www/wordpress-develop/public_html/src/wp-content/plugins/
 ln -s /vagrant/share/sources/amazonsimpleadmin/trunk amazonsimpleadmin
 ln -s /vagrant/share/sources/amazon-simple-affiliate/trunk/amazon-simple-affiliate amazon-simple-affiliate
 ln -s /vagrant/share/sources/post-status-notifier/trunk/post-status-notifier post-status-notifier
+ln -s /vagrant/share/sources/post-status-notifier/trunk/post-status-notifier-lite post-status-notifier-lite
